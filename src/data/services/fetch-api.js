d19d48631c20f4500b2171096c404f0a6786f562
Luiza Carvalho <111703755+luizalsc@users.noreply.github.com>
2023-03-15 19:29:32 -0300

    Update fetch-api.js

--- src/data/services/fetch-api.js
+++ src/data/services/fetch-api.js
@@ -2,7 +2,7 @@
 
 async function getTickers(stockTickers) {
               
-    const response = await fetch(`https://api.polygon.io/v3/reference/tickers/${stockTickers}?apiKey=Di9sCRa_Bj2l8cpNdcSXk4E3rpAp1aFP`)
+    const response = await fetch(`https://api.polygon.io/v3/reference/tickers/${stockTickers}?apiKey=insertKey`)
 
     return(await response.json())
 }
