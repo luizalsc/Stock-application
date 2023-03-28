6acfbf69fe91e74c213e639120aa55039262252b
Luiza Carvalho <luizacarvalho.arq@gmail.com>
2023-03-28 16:17:26 -0300

    WIP make portifolio form work

--- src/pages/portifolio.js
+++ src/pages/portifolio.js
@@ -6,42 +6,45 @@ import { useState } from 'react'
 
 const PortifolioForm = ({contributionValue, stockInfo}) => {
 
+    const userStocks = useSelector(state => state.userStocks)
+
     const [contribution, setContribution] = useState({
         amount: ''
     })
     const [stockInfos, setStockInfos] = useState({
-        name: '',
-        price:''
+        ticker: ''
     })
-    const [portifolio, setPortifolio] = useState({})
+
+    const [percentual, setPercentual] = useState({
+        precentual: ''
+    })
+
+    const [portifolio, setPortifolio] = useState([])
 
     const handleContributionChange = (event) => {
-        setContribution({
-            amount: event.target.value
-        })
+        setContribution({amount: event.target.value})
     }
 
     const handleStockInfoChange = (event) => {
-        setStockInfos({
-            name: event.target.value,
-            price:''
+        console.log(event.target.value)
+        setStockInfos({ticker: event.target.value})
+    }
+
+    const handlePercentualChange = (event) => {
+        setPercentual ({
+            percentual: event.target.value
         })
+    }
+
+    const addToPortifolio = () => {
+      
+        setPortifolio([...portifolio, {stockInfos, percentual}])
     }
 
     const handleSubmit = (event) => {
         event.preventDefault()
 
-        setPortifolio({ stock: stockInfos.name, contribution: contribution })
-        console.log(portifolio)
-
-        if(portifolio == {}){
-            return 
-        }
-        return (
-            <StockList portifolio={portifolio}/>
-        )
-
-
+        console.log('port final', portifolio)
     }
             
 
@@ -56,39 +59,36 @@ const PortifolioForm = ({contributionValue, stockInfo}) => {
 
                 <label htmlFor='stocks'>Selecione as ações desejadas</label>
                 <br/>
-                <StocksSelector/>
-                <StockOptions onChange={handleStockInfoChange} value={stockInfo}/>
+                <div>
+                    <select name='stocks' onChange={handleStockInfoChange} >
+                        {userStocks.length > 0 ? (
+                            userStocks.map((stock, index) => (
+                                <option  key={index} value={stock.cardStocks.ticker} name={stock.cardStocks.name}>
+                                    {stock.cardStocks.name}
+                                </option>                       
+                        ))
+                        ) : (
+                            <li>Nenhuma ação encontrada</li>
+                        )}
+                    </select>
+                    <input type='number' placeholder='100' id='number' required onChange={handlePercentualChange}></input>
+                    <label htmlFor='number'>%</label>
+                    <button onClick={addToPortifolio}>+</button>
+                    <br/><br/>
+                </div>
+                <button type='submit'>Enviar</button>
+                
                 <br/>
                 <br/><br/>
-                <button type='submit'>Submit</button>
+                
             </form> 
               
         </>
     )
 }
 
-const StockList = (portifolio) => {
-
-    return(
-        <ul>
-            {portifolio.map((stock, index) => (
-                <li key={index}>
-                    <p>{stock.stock}</p>
-                </li>
-            ))}
-
-        </ul>
-    )
-
-}
-
-
 const Portifolio = () => {
-
-    const userStocks = useSelector(state => state.userStocks)
-
-    const dispatch = useDispatch()
-        
+   
     return(
         <>
 
@@ -100,123 +100,3 @@ const Portifolio = () => {
 }
 
 export { Portifolio }
-
-const StockOptions = () => {
-
-    const userStocks = useSelector(state => state.userStocks)
-
-    const handleCheckboxChange = (e) => {
-
-        const selectedStock = e.target.value
-        console.log(selectedStock)
-        
-        return((prev)=> 
-            prev.some(data => data === selectedStock ) ? 
-            prev.filter(data => data !== selectedStock ) :  
-            [...prev, selectedStock]
-      )   
-
-    }
-
-    return(
-        <>
-            <ul id='stocks'>
-                {userStocks.length > 0 ? (
-                    userStocks.map((stock, index) => (
-                    <li key={index}>
-                        <input type='checkbox' value={stock.cardStocks.name} id={stock.index} onChange={handleCheckboxChange}/>
-                        <label htmlFor={stock.index}>{stock.cardStocks.name}</label>
-                    </li>
-                ))
-                ) : (
-                    <li>Nenhuma ação encontrada</li>
-                )}
-            </ul>
-            
-            <input type='number' placeholder='100' id='number'></input>
-            <label htmlFor='number'>%</label>
-            
-        </>
-        )
-
-}
-
-const StocksSelector = () => {
-
-    const userStocks = useSelector(state => state.userStocks)
-
-    // const [inputs, setInputs] = useState({
-    //     selectedstock: ''
-    // })
-
-    // const handleIpuntChange = (event) => {
-    //     setInputs({
-    //         selectedstock: event.target.value
-    //     })
-    // }
-    // const addNewSelectStock = (newStock) => {
-    //     console.log(inputs)
-    // }
-
-
-    return(
-        <>  
-            
-                <select name='stocks'>
-                    {userStocks.length > 0 ? (
-                        userStocks.map((stock, index) => (
-                            <option key={index} value={stock} name={stock.cardStocks.name}>
-                                {stock.cardStocks.name}
-                            </option>                       
-                    ))
-                    ) : (
-                        <li>Nenhuma ação encontrada</li>
-                    )}
-                </select>
-                <input type='number' placeholder='100' id='number' required></input>
-                <label htmlFor='number'>%</label>
-                <button onClick={()=>{}}>Enviar</button>
-            
-        </>
-        )
-
-}
-//-------------- FORM ORIGINAL ------------
-// const PortifolioForm = () => {
-
-//     const dispatch = useDispatch()
-
-//     const [inputs, setInputs] = useState({
-//         contributionAmount: ''
-//     })
-
-//     const handleIpuntChange = (event) => {
-//         setInputs({
-//             contributionAmount: event.target.value
-//         })
-//     }
-
-//     const handleSubmit = (event) => {
-//         event.preventDefault()
-//         return(inputs.contributionAmount)
-//     }
-
-//     return(
-//         <>
-//             <form onSubmit={handleSubmit}>
-//                 <label htmlFor='contribution'>Insira o valor que quer aportar</label>
-//                 <br/>
-//                 <input type='number' onChange={handleIpuntChange} value={inputs.contributionAmount} placeholder='Valor total do aporte' id='contribution'></input>
-//                 <button type='submit'>Enviar</button>
-//             </form> 
-//                 <br/><br/>
-//                 <label htmlFor='stocks'>Selecione as ações desejadas</label>
-//                 <br/>
-//                 <StocksSelector/>
-//                 <StockOptions/>
-//                 <br/>
-//                 <br/><br/>
-              
-//         </>
-//     )
-// }
