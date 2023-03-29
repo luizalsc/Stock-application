7b8b247870d19ffb9505b81e90f2532d6c646716
Luiza Carvalho <luizacarvalho.arq@gmail.com>
2023-03-29 17:44:05 -0300

    WIP implement validation at portifolio form - percentual input

--- src/pages/portifolio.js
+++ src/pages/portifolio.js
@@ -4,7 +4,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { useState } from 'react'
 
 
-const PortifolioForm = ({contributionValue, stockInfo}) => {
+const PortifolioForm = ({contributionValue, stockInfo, portifolioInfo}) => {
 
     const userStocks = useSelector(state => state.userStocks)
 
@@ -26,31 +26,44 @@ const PortifolioForm = ({contributionValue, stockInfo}) => {
     }
 
     const handleStockInfoChange = (event) => {
-        console.log(event.target.value)
+        console.log(event)
         setStockInfos({ticker: event.target.value})
     }
 
     const handlePercentualChange = (event) => {
-        setPercentual ({
-            percentual: event.target.value
-        })
+
+
+        const total = portifolio.reduce((total, stock) => total + parseInt(stock.percentual.percentual), 0)
+        const currentValue = parseInt(event.target.value) + total
+
+        if(currentValue > 100 && total < 100){
+            alert(`Digite um valor inteiro até ${100-total}`)
+            event.target.value = 100-total
+        }else if(total === 100){
+            alert(`Você já usou todo seu aporte`)
+            event.target.value = 0
+        }
+        
+        setPercentual ({percentual: event.target.value})  
     }
 
     const addToPortifolio = () => {
-      
+
         setPortifolio([...portifolio, {stockInfos, percentual}])
     }
 
     const handleSubmit = (event) => {
         event.preventDefault()
 
-        console.log('port final', portifolio)
+        console.log(portifolio)
+
+        return(portifolio)
     }
             
 
     return(
         <>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} value={portifolioInfo}>
                 <label htmlFor='contribution'>Insira o valor que quer aportar</label>
                 <br/>
 
@@ -60,11 +73,11 @@ const PortifolioForm = ({contributionValue, stockInfo}) => {
                 <label htmlFor='stocks'>Selecione as ações desejadas</label>
                 <br/>
                 <div>
-                    <select name='stocks' onChange={handleStockInfoChange} >
+                    <select name='stocks' onChange={handleStockInfoChange}>
                         {userStocks.length > 0 ? (
                             userStocks.map((stock, index) => (
-                                <option  key={index} value={stock.cardStocks.ticker} name={stock.cardStocks.name}>
-                                    {stock.cardStocks.name}
+                                <option key={index} value={stock.cardStocks.ticker} name={stock.cardStocks.ticker}>
+                                    {stock.cardStocks.name} - {stock.stocksCLosePrice.close}
                                 </option>                       
                         ))
                         ) : (
@@ -73,16 +86,15 @@ const PortifolioForm = ({contributionValue, stockInfo}) => {
                     </select>
                     <input type='number' placeholder='100' id='number' required onChange={handlePercentualChange}></input>
                     <label htmlFor='number'>%</label>
-                    <button onClick={addToPortifolio}>+</button>
+                    <button onClick={addToPortifolio} type='reset'>+</button>
                     <br/><br/>
                 </div>
                 <button type='submit'>Enviar</button>
-                
                 <br/>
                 <br/><br/>
-                
             </form> 
-              
+            
+            <Wallet portifolioInfo={portifolio}/>
         </>
     )
 }
@@ -100,3 +112,29 @@ const Portifolio = () => {
 }
 
 export { Portifolio }
+
+const Wallet = (props) => {
+
+    const userStocks = useSelector(state => state.userStocks)
+
+    return(
+        <ul>
+            {props.portifolioInfo.length > 0 ? (
+                props.portifolioInfo.map((stock, index) => (
+                    <li key={index} value={stock.stockInfos.ticker}>
+                        <p>{stock.stockInfos.ticker} - {stock.percentual.percentual}%</p>
+                    </li>                       
+            ))
+            ) : (
+                <li>Adicione suas ações</li>
+            )}
+        </ul>
+
+
+    )
+
+    
+}
+
+
+// const summation = portifolioInfo.reduce((summation, number) => summation + number.percentual.percentual, 0)
