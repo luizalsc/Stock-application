1a1fa6981b68fd4c369e83e7115b4a85f188d8f1
Luiza Carvalho <luizacarvalho.arq@gmail.com>
2023-04-18 09:23:18 -0300

    Implement new validations to research field and aply toUpperCase method

--- src/data/hooks/components/card/stock-card.js
+++ src/data/hooks/components/card/stock-card.js
@@ -9,11 +9,20 @@ const StockCard = () => {
         cardStocks: useSelector(state => state.cardStocks.results),
         stocksCLosePrice: useSelector(state => state.stockDetails)
     }
-    console.log(initialStock.stocksCLosePrice)
+    const userStocks = useSelector(state => state.userStocks)
     const dispatch = useDispatch()
 
-    if(initialStock.cardStocks === undefined){
+    const handleClick = (event)=>{
+        const value = event.target.value
+        const repeatedSotcks = userStocks.filter(item => item.cardStocks.ticker === value)
+        if(repeatedSotcks.length > 0){
+            alert(`Você já adicionou ${value} à sua carteira. Cada ação só pode ser adicionada uma única vez`)
+            return
+        }
+        dispatch(addStocksToPortifolio(initialStock))
+    }
 
+    if(initialStock.cardStocks === undefined){
         return(
             <>
                 <p>Pesquise uma sigla de ação</p>  
@@ -30,7 +39,7 @@ const StockCard = () => {
                         <h3>Preço da ação</h3>
                         <p>{initialStock.stocksCLosePrice.close}</p>
                     </div>
-                    <button onClick={() => {dispatch(addStocksToPortifolio(initialStock))}}>Adicionar ação à minha carteira</button>
+                    <button onClick={handleClick} value={initialStock.cardStocks.ticker}>Adicionar ação à minha carteira</button>
                     
                 </section>   
             </>
