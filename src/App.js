08fd0e7cbc66013a31de5a1ec0d6fbe74a092017
Luiza Carvalho <luizacarvalho.arq@gmail.com>
2023-05-16 14:43:36 -0300

    Install tailwind and create style directory

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './style/reset.css'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { store, persistor } from './data/store/store'
