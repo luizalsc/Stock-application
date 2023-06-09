334db9ab5c7870ecf12235d291756b6ec4a66aee
Luiza Carvalho <luizacarvalho.arq@gmail.com>
2023-06-09 16:15:22 -0300

    Refactor the tests of StocksPortfolio/index.test.js file and create an testing-utils.js for it

---
+++ src/components/StocksPortfolio/testing-utils.js
@@ -0,0 +1,14 @@
+import configureStore from 'redux-mock-store'
+
+function createMockStore () {
+  const mockStore = configureStore([])
+  const store = mockStore({
+    userStocks: [
+      { cardStocks: { name: 'Test Company', ticker: 'TTT', description: 'Lorem Ipsum' }, stocksCLosePrice: { close: 100.00 } },
+      { cardStocks: { name: 'Test Company 2', ticker: 'TST2', description: 'Lorem Ipsum 2' }, stocksCLosePrice: { close: 200.00 } }
+    ]
+  })
+  return store
+}
+
+export { createMockStore }
